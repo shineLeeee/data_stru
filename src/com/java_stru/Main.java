577f6b8a72a53ebbf3829f93030c90a0dbfd3628
shineLeeee <a40357321311@163.com>
2017-03-02 16:37:06 +0800

    1

--- src/com/java_stru/Main.java
+++ src/com/java_stru/Main.java
@@ -1,16 +1,25 @@
 package com.java_stru;
 
 import java.util.*;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		SeqList sl=new SeqList(100);
+		for(int i=0;i<100;i++){
+			sl.insert(i, new Integer(i+1));
+		}
+		
+		ArrayList<String> ssl=new ArrayList<>();
+		ssl.add("world");
+		LinkedList<Integer> elk=new LinkedList<>();
+		
+		
+		System.out.print(sl.getData(5));
 		
 
 	}
+	
 
 }
