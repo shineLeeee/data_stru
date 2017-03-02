aece06fa1258d5d917ebcb672e557be151aaa7b7
shineLeeee <a40357321311@163.com>
2017-03-03 01:25:13 +0800

    first——complish

--- src/com/java_stru/Main.java
+++ src/com/java_stru/Main.java
@@ -68,20 +68,20 @@ public class Main {
 //        else if (i==0&&!(judge.isEmpty())) {
 //			System.out.println("左括号多了");
 //		}
-		//核心思想是用栈来保存一个由小到大的优先级
-		String pre="5*(3+4)-6";
+//		核心思想是用栈来保存一个由小到大的优先级
+		String pre="(3+4)*6+3*1+3*4";
 		char[] preArr=pre.toCharArray();
 		Stack<Character> operator=new Stack<Character>();//存放运算符
 		Stack<Character> formula=new Stack<Character>();//存放结果表
-		formula.push('#');//终止符
 		Map map=new HashMap<>();
-		map.put('*', 4);
-		map.put('/', 4);
-		map.put('+', 3);
-		map.put('-', 2);
+		map.put('*', 2);
+		map.put('/', 2);
+		map.put('+', 1);
+		map.put('-', 1);
 		map.put('#', 0);//设置优先级映射
-		
+		map.put('(', 0);
 	    Character former='#';
+	    char judge;
 		Pattern pattern=Pattern.compile("[0-9]*");
 		for (Character perChar : preArr) {
 //			System.out.println(pattern.matcher(perChar+"").matches());
@@ -89,6 +89,11 @@ public class Main {
 				formula.push(perChar);
 			}
 			else{
+				if(perChar=='('){//单独做一个判断是为了让左括号无条件进栈
+					operator.push('(');
+					former=perChar;
+					}
+				else if(perChar!=')'){
 				Character latter=perChar;
 				int latterInt=Integer.parseInt(map.get(latter).toString());
 				int formerInt=Integer.parseInt(map.get(former).toString());
@@ -97,18 +102,34 @@ public class Main {
 					former=perChar;
 				}
 				else {
-					formula.push(operator.pop());//后面比前面优先级低，把前面push进formula里，修改perChar为former
+					formula.push(operator.pop());//后面比前面优先级低，把前面push进formula里,当前进栈变former
 					operator.push(perChar);
 					former=perChar;
 					}
+				}
+				else {//当碰到右括号直接pop
+					do {
+						judge=operator.pop();
+//					    System.out.println(judge);
+					    formula.push(judge);
+					} while (judge=='(');
+					
+					judge='\0';
+					operator.pop();
+					if(!operator.isEmpty())
+					former=operator.lastElement();
+//					System.out.println("现在的最后一个是"+former);
+				}
 			}
 		}
-		for (Character character : operator) {//把剩余的运算符压入结果
-			formula.push(operator.pop());
+		while(!operator.isEmpty()){
+			System.out.println(formula.push(operator.pop()));
 		}
+		
 		for (Character character : formula) {//看结果
 			System.out.print(character);
 		}
+		
 		
 		
 		
