7ff5b7cf98e10e8fbd73135b08f66f04d2c6bc3e
shineLeeee <a40357321311@163.com>
2017-03-02 17:29:56 +0800

    3

--- src/com/java_stru/Main.java
+++ src/com/java_stru/Main.java
@@ -10,11 +10,22 @@ public class Main {
 		for(int i=0;i<100;i++){
 			sl.insert(i, new Integer(i+1));
 		}
-		
-		ArrayList<String> ssl=new ArrayList<>();
-		ssl.add("world");
+		//≤‚ ‘À≥–Ú±Ì
+//		ArrayList<String> ssl=new ArrayList<>();
+//		ssl.add("world");
+		//≤‚ ‘µ•¡¥±Ì
 		LinkedList<Integer> elk=new LinkedList<>();
 		
+		elk.addFirst(1);
+		elk.add(20);
+		elk.add(30);
+		elk.add(40);
+		elk.add(2,100);
+		elk.remove(1);
+		for(int x=0;x<3;x++){
+			System.out.println(elk.get(x));
+		}
+		Stack<String> stack=new Stack<>();
 		
 		System.out.print(sl.getData(5));
 		
