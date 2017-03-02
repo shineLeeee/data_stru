c6325190500287a889434c4639d18ec4b8ec9487
shineLeeee <a40357321311@163.com>
2017-03-02 20:24:01 +0800

    完整版

--- src/com/java_stru/Main.java
+++ src/com/java_stru/Main.java
@@ -38,19 +38,26 @@ public class Main {
 //		for (int i=stack.size();i>0;i--) {
 //			System.out.println(stack.pop());
 //			System.out.println(stack.get(i));
-		//括号匹配练习version1
-//		String a="a+(2*4)+()))";
+		/*括号匹配练习version2     核心思想,左括号入栈，右括号时左括号出栈，如果右括号时左为空右多，
+		 * 如果右括号时左不匹配顺序错,如果全部结束栈不空左多，否则全部正确*/
+//		String a="((}(]((a+(2*4)+())";//待匹配字符串//
 //		int i=a.length();
 //		Stack<Character> judge=new Stack<Character>();
 //		char[] m=a.toCharArray();
 //        for (char c : m) {
 //			if(c=='(' || c=='{'||c=='[')
 //				judge.push(c);
-//			if(c==')'||c==']'||c=='}'){
-//				if(!judge.isEmpty()) judge.pop();
-//				else {
+//			if(c==')'||c==']'||c=='}'){			
+//				if(judge.isEmpty()){
 //					System.out.println("右括号多了");
 //					break;
+//				}
+//				else {
+//				char seqJudge=judge.pop();
+//				if(!((c==')'&&seqJudge=='(')||(c==']'&&seqJudge=='[')||(c=='}'&&seqJudge=='{'))){
+//					System.out.println("括号次序不正确");
+//					break;
+//				}
 //				}
 //			}
 //				
@@ -60,11 +67,9 @@ public class Main {
 //        else if (i==0&&!(judge.isEmpty())) {
 //			System.out.println("左括号多了");
 //		}
-	    //括号匹配联系 version2
-		String a="a+(2*4)+()))";
-		int i=a.length();
-		Stack<Character> judge=new Stack<Character>();
-		char[] m=a.toCharArray();
+
+		
+		
 		
 		}
 		}
